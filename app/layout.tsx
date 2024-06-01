e681680cd4279271c32cfc7cfd25ad26f82fa0a2
mert-ozturk <mertozturk.seo.yonetim@gmail.com>
2024-06-01 21:21:37 +0300

    islem2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import RootProviders from "@/components/providers/RootProviders";
+ 
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +18,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html
+        lang="en"
+        className="dark"
+        style={{
+          colorScheme: "dark",
+        }}
+      >
+        <body className={inter.className}>
+          
+          <RootProviders>{children}</RootProviders>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
